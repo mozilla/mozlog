be6066c60b6f25a7883e8beab34cdff40d83ef8f
Julien Wajsberg <felash@gmail.com>
2020-05-18 17:24:31 +0200

    Change how the pretty formatter output its arguments

--- test/pretty_formatter.js
+++ test/pretty_formatter.js
@@ -33,30 +33,30 @@ describe('pretty formatter', function() {
   it('should serialize objects to JSON strings', function() {
     var obj = { foo: { bar: 'baz' }, quux: 'baz' };
     logger.info('json', obj);
-    assert.equal(lastLog, 'mozlog_pretty.test.pretty.INFO: json {"foo":{"bar":"baz"},"quux":"baz"}\n');
+    assert.equal(lastLog, 'INFO mozlog_pretty.test.pretty.json: {"foo":{"bar":"baz"},"quux":"baz"}\n');
   });
 
   it('should serialize circular objects', function() {
     var obj = { foo: {} };
     obj.foo.bar = obj;
     logger.info('jsoncircles', obj);
-    assert.equal(lastLog, 'mozlog_pretty.test.pretty.INFO: jsoncircles {"foo":{"bar":"[Circular]"}}\n');
+    assert.equal(lastLog, 'INFO mozlog_pretty.test.pretty.jsoncircles: {"foo":{"bar":"[Circular]"}}\n');
   });
 
   it('should serialize errors', function() {
     var err1 = new Error('foo');
     logger.error('errors', err1);
-    assert(lastLog.startsWith('mozlog_pretty.test.pretty.ERROR: errors Error: foo'));
+    assert(lastLog.startsWith('ERROR mozlog_pretty.test.pretty.errors: Error: foo'));
   });
 
   it('should serialize nested errors', function() {
     var err1 = new Error('foo');
     logger.error('errors', { obj: err1 });
-    assert.equal(lastLog, 'mozlog_pretty.test.pretty.ERROR: errors {"obj":"Error: foo"}\n');
+    assert.equal(lastLog, 'ERROR mozlog_pretty.test.pretty.errors: {"obj":"Error: foo"}\n');
   });
 
   it('should coerce messages to strings', function() {
     var out = logger.warn('message', 42);
-    assert.equal(lastLog, 'mozlog_pretty.test.pretty.WARN: message 42\n');
+    assert.equal(lastLog, 'WARN mozlog_pretty.test.pretty.message: 42\n');
   });
 });
